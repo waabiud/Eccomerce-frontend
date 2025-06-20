72537585c84cb339c671e65616e2dfc800109295
Wamalwa <155254269+waabiud@users.noreply.github.com>
2025-06-20 19:11:55 +0300

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  baseURL: "http://localhost:5000/api", // Must match your backend
+  baseURL: "http://localhost:5000/api", // Must match the backend
   withCredentials: true,                // Critical for cookie auth
 });
 
